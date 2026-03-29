9c7af381c2e7185fdd47d7bcbda87f7b08ce6524
luxecharms10-jpg <luxecharms1.0@gmail.com>
2026-03-29 04:10:39 -0300

    Update MainActivity.java

--- app/src/main/java/jogo/teste/com/MainActivity.java
+++ app/src/main/java/jogo/teste/com/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import jogo.teste.com.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
