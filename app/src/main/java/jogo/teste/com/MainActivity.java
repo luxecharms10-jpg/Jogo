f787405e061963ce1b00592ccc456c0ae08fb879
luxecharms10-jpg <luxecharms1.0@gmail.com>
2026-03-29 01:36:40 -0300

    Create MainActivity.java

---
+++ app/src/main/java/jogo/teste/com/MainActivity.java
@@ -0,0 +1,24 @@
+package jogo.teste.com;
+
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+
+public class MainActivity extends AppCompatActivity {
+  
+
+  }  
